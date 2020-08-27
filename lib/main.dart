a579779fbaaeff5eceac63ab0bf89efd3ddd53ad
Caroline <caroline_mendez@baylor.edu>
2020-08-27 16:41:37 -0500

    login/signup page

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:insta/screens/login_screen.dart';
+import 'package:insta/screens/signup_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Instragram Clone',
+        debugShowCheckedModeBanner: false,
+        home: LoginScreen(),
+        routes: {
+          LoginScreen.id: (context) => LoginScreen(),
+          SignupScreen.id: (context) => SignupScreen()
+        });
+  }
+}
